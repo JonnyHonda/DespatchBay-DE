989340d581be401c60f2391e327871b4f555c252
John Burrin <jonnyb3010@gmail.com>
2019-03-23 21:20:30 +0000

    Initial commit

---
+++ DespatchBay-DE/DataStorage.cs
@@ -0,0 +1,75 @@
+﻿using SQLite;
+
+/// <summary>
+/// This storage class is only to be used for offline storage of the API results to reduce the amount of calls require to the despatch bay systems. 
+/// </summary>
+namespace DespatchBay_DE
+{
+    class DataStorage
+    {
+    }
+
+    /// <summary>
+    /// Account.
+    /// </summary>
+    public class Account
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        public int ID { get; set; } // an auto increment data base ID
+
+        public int AccountID { get; set; } // the Client account ID as stored in Despatch Bay 
+        public string AccountName { get; set; }
+        public double Balance { get; set; }
+        public double AvailableBalance { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("[Account: ID={0}, AccountID={1}, AccountName={2}, Balance={3}, AvailableBalance={4}]", ID, AccountID, AccountName, Balance, AvailableBalance);
+        }
+
+    }
+
+    /// <summary>
+    /// SenderAddress
+    /// </summary>
+    public class SenderAddress
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        public int ID { get; set; } // an auto increment data base ID
+        public int ExternalID { get; set; } // the id as storded in the external system
+        public int SenderAddressID { get; set; } // the sender drress id in despatch bay
+        public int AddressID { get; set; } // our internal id of the address
+        public string SenderName { get; set; }
+        public string SenderTelephone { get; set; }
+        public string SenderEmail { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("[SenderAddress: ID={0}, ExternalID={1}, SenderAddressID={2}, AddressID={3}, SenderName={4}, SenderTelephone={5},  SenderEmail={6}]", ID, ExternalID, SenderAddressID, AddressID, SenderName, SenderTelephone, SenderEmail);
+        }
+    }
+
+    /// <summary>
+    /// Address
+    /// </summary>
+    public class AddressService
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        public int ID { get; set; } // an auto increment data base ID
+        public string CompanyName { get; set; }
+        public string Street { get; set; }
+        public string Locality { get; set; }
+        public string TownCity { get; set; }
+        public string County { get; set; }
+        public string PostalCode { get; set; }
+        public string CountryCode { get; set; }
+        public override string ToString()
+        {
+            return string.Format("[Address: ID={0}, CompanyName={1}, Street={2}, Locality={3}, TownCity={4}, County={5}, PostalCode={6}, CountryCode={6}]", ID, CompanyName, Street, Locality, TownCity, County, PostalCode, CountryCode);
+        }
+    }
+
+}
---
+++ DespatchBay-DE/DespatchBayAPI.cs
@@ -0,0 +1,81 @@
+﻿using DespatchBay_DE.com.despatchbay.api.account;
+using DespatchBay_DE.com.despatchbay.api.addressing;
+using DespatchBay_DE.com.despatchbay.api.shipping;
+using DespatchBay_DE.com.despatchbay.api.tracking;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DespatchBay_DE
+{
+
+    class DespatchBayAPI
+    {
+        private  NetworkCredential netCredential = null;
+        public DespatchBayAPI(string apiuser, string apikey)
+        {
+            netCredential = new NetworkCredential(apiuser, apikey);
+        }
+
+        public AccountService AuthenticateWithDespatchBay(AccountService Service)
+        {
+            // Create the service of type Shipping service
+
+            Service.RequestEncoding = System.Text.Encoding.UTF8;
+            Uri uri = new Uri(Service.Url);
+            ICredentials credentials = SetAuthType(uri);
+            // Apply the credentials to the service
+            Service.Credentials = credentials;
+            return Service;
+        }
+
+
+        public AddressingService AuthenticateWithDespatchBay(AddressingService Service)
+        {
+            // Create the service of type Shipping service
+
+            Service.RequestEncoding = System.Text.Encoding.UTF8;
+            Uri uri = new Uri(Service.Url);
+            ICredentials credentials = SetAuthType(uri);
+            // Apply the credentials to the service
+            Service.Credentials = credentials;
+            return Service;
+        }
+
+        public ShippingService AuthenticateWithDespatchBay(ShippingService Service)
+        {
+            // Create the service of type Shipping service
+
+            Service.RequestEncoding = System.Text.Encoding.UTF8;
+            Uri uri = new Uri(Service.Url);
+            ICredentials credentials = SetAuthType(uri);
+            // Apply the credentials to the service
+            Service.Credentials = credentials;
+            return Service;
+        }
+
+        public TrackingService AuthenticateWithDespatchBay(TrackingService Service)
+        {
+            // Create the service of type Shipping service
+
+            Service.RequestEncoding = System.Text.Encoding.UTF8;
+            Uri uri = new Uri(Service.Url);
+            ICredentials credentials = SetAuthType(uri);
+            // Apply the credentials to the service
+            Service.Credentials = credentials;
+            return Service;
+        }
+
+        private ICredentials SetAuthType(Uri uri)
+        {
+            return netCredential.GetCredential(uri, "Basic");
+        }
+
+    }
+
+
+}
